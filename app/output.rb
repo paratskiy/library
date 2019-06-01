5bc998d3ba399756f3039415240559ee005089ec
Bogdan Paratskiy <bog4242@gmail.com>
2019-06-01 10:22:45 +0300

    add output

---
+++ app/output.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'dependencies/dependencies.rb'
+
+# #
+class Output < Statistic
+  def show_top_readers(count = 1)
+    top_readers = Statistic.new.select_top_readers(count)
+    top_readers.each { |reader| puts "Top reader is #{reader[0].name}" }
+  end
+
+  def show_top_books(count = 1)
+    top_books = Statistic.new.select_top_books(count)
+    top_books.each { |book| puts "Top book is #{book[0].title}" }
+  end
+
+  def show_reader_of_top_books(quantity = 3)
+    reader_of_top_books = Statistic.new.select_reader_of_top_books(quantity)
+    reader_of_top_books.each { |reader| puts "Reader of top books is #{reader.name}" }
+  end
+end
